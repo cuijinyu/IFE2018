fd9f9131504c922c52f62c36be1b71e9216f4dd9
崔晋瑜 <2510271615@qq.com>
2018-05-04 23:10:35 +0800

    更新了打包

--- mvvm/task3.3/webpack.prod.js
+++ mvvm/task3.3/webpack.prod.js
@@ -1,56 +1,62 @@
 const WebpackParalleUglifyPlugin = require("webpack-parallel-uglify-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
 module.exports = {
-    entry: __dirname + '/src/index.js',
     mode:"production",
-    devtool:"inline-source-map",
-    output: {
-        path:__dirname + '/dist',
-        filename: "bundle.js"
+    entry: {
+        app: path.resolve(__dirname, './src/index.js'),
     },
-    loader: {
-        rules:[
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
             {
-                test: /\.san$/,
-                loader: 'san-loader',
+                test: /\.js$/,
+                exclude: [path.resolve(__dirname, './node_modules')],
+                use: 'babel-loader',
             },
             {
-                test:/(\.jsx|\.js)$/,
-                use:{
-                    loader:"bable-loader",
-                    options:{
-                        preset:["env","react"]
-                    }
-                },
-                exclude:/node_modules/
+                // 图片加载器
+                test:/\.(png|jpg|gif|jpeg)$/,
+                use:'url-loader?limit=2048'
             },
             {
-                test:/\.css$/,
-                use:[
-                    {
-                        loader:"style-loader"
-                    },
-                    {
-                        loader:"css-loader"
-                    }
-                ]
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test:/\.san$/,
+                use:'san-loader'
             }
-        ]
+        ],
+    },
+    resolve: {
+        alias: {
+            san:
+                NODE_ENV === 'production'
+                    ? 'san/dist/san.js'
+                    : 'san/dist/san.dev.js',
+        },
     },
     plugins: [
-        new WebpackParalleUglifyPlugin({
-            uglifyJS: {
-                mangle: false,
-                output: {
-                    beautify: false,
-                    comments: false
-                },
-                compress: {
-                    warnings: false,
-                    drop_console: true,
-                    collapse_vars: true,
-                    reduce_vars: true
-                }
-            }
-        })
+        // new WebpackParalleUglifyPlugin({
+        //     uglifyJS: {
+        //         mangle: false,
+        //         output: {
+        //             beautify: false,
+        //             comments: false
+        //         },
+        //         compress: {
+        //             warnings: false,
+        //             drop_console: true,
+        //             collapse_vars: true,
+        //             reduce_vars: true
+        //         }
+        //     }
+        // })
     ]
 }
